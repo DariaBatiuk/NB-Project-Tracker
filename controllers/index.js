8cee3061f3c6e184c07f4319304f05dcd25a041c
Ryguy <ryan70297@gmail.com>
2023-02-14 18:18:04 -0400

    added routes and pages to views

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const newPost = require('./newPost');
 const apiRoutes = require('./api');
-
+const myProfile = require('./myprofile');
+const allPosts = require('./allposts');
 router.use('/', homeRoutes);
 router.use('/newpost', newPost);
+router.use('/myprofile', myProfile)
+router.use('/posts', allPosts)
 router.use('/api' ,apiRoutes )
 module.exports = router;
