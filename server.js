86c7cd599e8e47ee44bdf53d5a1668ef41e19fb8
Ryguy <ryan70297@gmail.com>
2023-02-14 18:35:34 -0400

    have to commit to pull

--- server.js
+++ server.js
@@ -2,18 +2,28 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
+const path = require('path');
+
 //TODO: Uncomment to make use of database, once set up
-//const sequelize = require('./config/connection');
-const app = express();
-const PORT = process.env.PORT || 3001;
+// const sequelize = require('./config/connection');
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
-app.use(express.static("public"));
+
 //Setup routes to the Server
 //Look at /controllers folder
-app.use("/", routes);
+app.use(routes);
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 
 //TODO: Uncomment to make use of database, once set up
 // sequelize.sync({force: false}).then(()=>{
