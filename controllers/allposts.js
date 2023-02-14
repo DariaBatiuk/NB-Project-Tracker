f2b6b1b3e453aaf36ab13dd7b47a212b5bf9effd
Ryguy <ryan70297@gmail.com>
2023-02-14 19:10:19 -0400

    new pages and routes to render

--- controllers/allposts.js
+++ controllers/allposts.js
@@ -1,9 +1,25 @@
+const { Project, User } = require('../models');
 
 const router = require('express').Router();
 router.get('/', async (req, res) => {
     // Send the rendered Handlebars.js template back as the response
-    res.render('allposts');
-  });
+   try {
+    const postData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render('allposts', { posts });
+  } catch (err) {
+        res.status(500).json(err);
+        }
+    });
+
   
   module.exports = router;
   
--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const routes = require("./controllers");
 const path = require('path');
 
 //TODO: Uncomment to make use of database, once set up
-// const sequelize = require('./config/connection');
-// const app = express();
-// const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
@@ -26,11 +26,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 //TODO: Uncomment to make use of database, once set up
-// sequelize.sync({force: false}).then(()=>{
-//     app.listen(PORT, () => {
-//         console.log(`Server is listening at http://localhost:${PORT}`)
-//     });
-// });
+sequelize.sync({force: false}).then(()=>{
+    app.listen(PORT, () => {
+        console.log(`Server is listening at http://localhost:${PORT}`)
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
