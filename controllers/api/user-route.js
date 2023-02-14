146325b4084427dc2e043c5b6585beac04b6fb4b
Liibaan Halane <liibaanhalane003@gmail.com>
2023-02-14 19:54:00 -0400

    added format for addpost post route

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 
 router.post('/newpost', async (req, res) => {
-// db query here
-
-
+const { title, description, skills, github_repo, date_created, email } = req.body;
+  Project.create({ title, description, skills, github_repo, date_created, email })
+    .then(post => {
+      res.redirect(`/projects/${project.id}`);
+    })
+    .catch(error => {
+      console.error(error);
+      res.sendStatus(500);
+    });
 });
 
 router.get('/user/:name', (req, res) => {
