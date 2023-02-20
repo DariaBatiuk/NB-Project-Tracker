80d94814f0d327fa3cb6b2d660b57fc75de5d0e1
Ryguy <ryan70297@gmail.com>
2023-02-20 19:08:45 -0400

    tracking

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,13 @@ const homeRoutes = require('./homeRoutes');
 const newPost = require('./newPost');
 const apiRoutes = require('./api');
 const myprofile = require('./profile');
+const profilePublic = require('./publicProfile');
 
 router.use('/', homeRoutes);
 router.use('/newpost', newPost);
 router.use('/api' ,apiRoutes );
 router.use('/profile', myprofile)
+router.use('/publicprofile', profilePublic)
+    
+
 module.exports = router;
